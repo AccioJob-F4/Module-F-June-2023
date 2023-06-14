45b7f50e0760f5297d47f43d41e3edb3d270727e
RgnDunes <divyansh.singh@razorpay.com>
2023-06-15 02:34:08 +0530

    Add Calculator React Solution

--- Assignment Solutions/calculator_react/src/App.js
+++ Assignment Solutions/calculator_react/src/App.js
@@ -24,62 +24,47 @@ const App = () => {
   };
 
   return (
-    <div className="calculator">
-      <div className="display">
-        <input type="text" value={display} readOnly />
-      </div>
-      {/* <div className="buttons">
-        <button onClick={() => handleClick("7")}>7</button>
-        <button onClick={() => handleClick("8")}>8</button>
-        <button onClick={() => handleClick("9")}>9</button>
-        <button onClick={() => handleClick("/")}>/</button>
-        <button onClick={() => handleClick("4")}>4</button>
-        <button onClick={() => handleClick("5")}>5</button>
-        <button onClick={() => handleClick("6")}>6</button>
-        <button onClick={() => handleClick("*")}>*</button>
-        <button onClick={() => handleClick("1")}>1</button>
-        <button onClick={() => handleClick("2")}>2</button>
-        <button onClick={() => handleClick("3")}>3</button>
-        <button onClick={() => handleClick("-")}>-</button>
-        <button onClick={() => handleClick("0")}>0</button>
-        <button onClick={() => handleClick(".")}>.</button>
-        <button onClick={() => handleClick("=")}>=</button>
-        <button onClick={() => handleClick("+")}>+</button>
-        <button onClick={() => handleClick("C")}>C</button>
-      </div> */}
-      {/* <div className="result">
-        <p>{result}</p>
-      </div> */}
+    <div className="app">
+      <div className="calculator">
+        <div className="display">
+          <input type="text" value={display} readOnly />
+        </div>
 
-      <table>
-        <tr>
-          <button onClick={() => handleClick("C")}>C</button>
-          <button onClick={() => handleClick("/")}>/</button>
-          <button onClick={() => handleClick("*")}>*</button>
-          <button onClick={() => handleClick("-")}>-</button>
-        </tr>
-        <tr>
-          <button onClick={() => handleClick("7")}>7</button>
-          <button onClick={() => handleClick("8")}>8</button>
-          <button onClick={() => handleClick("9")}>9</button>
-          <button onClick={() => handleClick("+")}>+</button>
-        </tr>
-        <tr>
-          <button onClick={() => handleClick("4")}>4</button>
-          <button onClick={() => handleClick("5")}>5</button>
-          <button onClick={() => handleClick("6")}>6</button>
-        </tr>
-        <tr>
-          <button onClick={() => handleClick("1")}>1</button>
-          <button onClick={() => handleClick("2")}>2</button>
-          <button onClick={() => handleClick("3")}>3</button>
-        </tr>
-        <tr>
-          <button onClick={() => handleClick("0")}>0</button>
-          <button onClick={() => handleClick(".")}>.</button>
-          <button onClick={() => handleClick("=")}>=</button>
-        </tr>
-      </table>
+        <table>
+          <tr>
+            <td onClick={() => handleClick("C")}>C</td>
+            <td onClick={() => handleClick("/")}>/</td>
+            <td onClick={() => handleClick("*")}>*</td>
+            <td onClick={() => handleClick("-")}>-</td>
+          </tr>
+          <tr>
+            <td onClick={() => handleClick("7")}>7</td>
+            <td onClick={() => handleClick("8")}>8</td>
+            <td onClick={() => handleClick("9")}>9</td>
+            <td rowSpan={2} onClick={() => handleClick("+")}>
+              +
+            </td>
+          </tr>
+          <tr>
+            <td onClick={() => handleClick("4")}>4</td>
+            <td onClick={() => handleClick("5")}>5</td>
+            <td onClick={() => handleClick("6")}>6</td>
+          </tr>
+          <tr>
+            <td onClick={() => handleClick("1")}>1</td>
+            <td onClick={() => handleClick("2")}>2</td>
+            <td onClick={() => handleClick("3")}>3</td>
+            <td rowSpan={2} onClick={() => handleClick("=")}>
+              =
+            </td>
+          </tr>
+          <tr>
+            <td onClick={() => handleClick("0")}>0</td>
+            <td onClick={() => handleClick(".")}>.</td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 };
