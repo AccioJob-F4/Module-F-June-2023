1479fe9945f9faed3ab55e4965b701859d2cba2f
RgnDunes <divyansh.singh@razorpay.com>
2023-06-20 21:50:18 +0530

    Refactor middleware code

--- ReactJS/session-04/src/customMiddleware.js
+++ ReactJS/session-04/src/customMiddleware.js
@@ -1,7 +1,9 @@
 export const customMiddleware = (store) => (next) => (action) => {
   console.log("MiddleWare : ", action, store.getState());
 
-  const updatedActionObj = { ...action, id: "JHFVJHVJH" };
+  // const updatedActionObj = { ...action, id: "JHFVJHVJH" };
 
-  next(updatedActionObj);
+  action.id = "JHFVJHVJH";
+
+  next(action);
 };
